5cd7a65618ab23b39852215baf3eeb337312ef88
Neoscripter <sange0337@gmail.com>
2026-04-04 11:39:33 +0800

    Fix:make signup and login popup disappear on the esc key press and on click outside the form

--- assets/src/js/popup.js
+++ assets/src/js/popup.js
@@ -1,7 +1,6 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     if (!popupData.is_user_logged_in) {
-
-        $('.event-join').on('click', event => {
+        $('.event-join').on('click', (event) => {
             event.preventDefault();
             $('#reg-popup').fadeIn();
             $('.signup-form').fadeIn();
@@ -13,47 +12,74 @@ jQuery(document).ready(function($) {
         //     $('.signup-form').fadeIn();
         // });
 
-        $('.custom-dropdown-button').on('click', event => {
+        $('#reg-popup').on('click', (event) => {
+            event.stopPropagation();
+            $('#reg-popup').fadeOut();
+            $('.signup-form').fadeOut();
+        });
+
+        $('#login-popup').on('click', (event) => {
+            event.stopPropagation();
+            $('#login-popup').fadeOut();
+            $('.login-form').fadeOut();
+        });
+
+        $(document).on('keydown', function (e) {
+            if (e.key === 'Escape') {
+                $('#login-popup').fadeOut();
+                $('.login-form').fadeOut();
+                $('#reg-popup').fadeOut();
+                $('.signup-form').fadeOut();
+            }
+        });
+
+        $('.signup-form').on('click', (event) => {
+            event.stopPropagation();
+        });
+
+        $('.login-form').on('click', (event) => {
+            event.stopPropagation();
+        });
+
+        $('.custom-dropdown-button').on('click', (event) => {
             event.preventDefault();
             $('#reg-popup').fadeIn();
             $('.signup-form').fadeIn();
         });
 
-        $('.view-all-button').on('click', event => {
+        $('.view-all-button').on('click', (event) => {
             event.preventDefault();
             $('#reg-popup').fadeIn();
             $('.signup-form').fadeIn();
         });
 
-        $('#login-btn').on('click', function() {
+        $('#login-btn').on('click', function () {
             $('.signup-form').fadeOut();
             $('.login-form').fadeIn();
             $('.error-messages').text('');
         });
-    
-        $('#signup-btn').on('click', function() {
+
+        $('#signup-btn').on('click', function () {
             $('.login-form').fadeOut();
             $('.signup-form').fadeIn();
             $('.error-messages').text('');
         });
 
-
-        $('#header-login-btn').on('click', function() {
+        $('#header-login-btn').on('click', function () {
             $('#reg-popup').fadeIn();
             $('.signup-form').fadeOut();
             $('.login-form').fadeIn();
             $('.close-reg').show();
         });
-    
-        $('#header-signup-btn').on('click', function() {
+
+        $('#header-signup-btn').on('click', function () {
             $('#reg-popup').fadeIn();
             $('.login-form').fadeOut();
             $('.signup-form').fadeIn();
             $('.close-reg').show();
         });
 
-
-        $('.close-reg').on('click', function() {
+        $('.close-reg').on('click', function () {
             $('#reg-popup').fadeOut();
         });
     }
